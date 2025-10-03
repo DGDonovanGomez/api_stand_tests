1a4cf4fa9dd25933981a03153da6aac9ee851390
Donovan Gómez <donovan.gomez3863@gmail.com>
2025-10-02 21:30:47 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-46bc58c2-3f79-4857-aa6b-aacd5fe85c76.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
